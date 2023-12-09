bf8ae2f4d67d61a2a52a608b1c35bf14e253b8a7
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2023-12-09 14:17:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import docx2txt
+from streamlit_card import card
+import base64
 
 st.title(' ⏳ Sandbox')
 
@@ -7,3 +8,27 @@ with st.sidebar:
     st.markdown('''
     <img src="https://executive-education.minesparis.psl.eu/wp-content/themes/psl/images/logo-header.svg">
     ''', unsafe_allow_html=True)
+
+
+
+with open('bg-img.png', "rb") as f:
+    data = f.read()
+    encoded = base64.b64encode(data)
+data = "data:image/png;base64," + encoded.decode("utf-8")
+
+
+hasClicked = card(
+  title="Button 1",
+  text="Some description",
+  image=data,
+  on_click=lambda: print('hi'),
+  key='first'
+)
+
+hasClicked2 = card(
+  title="Button 2",
+  text="Some description",
+  image=data,
+  on_click=lambda: print('hi again'),
+  key='second'
+)
