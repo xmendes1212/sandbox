c13d2b4951f4e9d02f0e6a1d0d88dd9ff437c70d
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2023-12-09 13:49:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ st.title(' ⏳ Sandbox')
 
 with st.sidebar:
     st.markdown('''
-    <img src="https://executive-education.minesparis.psl.eu/wp-content/themes/psl/images/logo-header.svg"
+    <img src="https://executive-education.minesparis.psl.eu/wp-content/themes/psl/images/logo-header.svg">
     ''', unsafe_allow_html=True)
