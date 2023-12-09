e50c09b9553674891526721cd662f124db56370f
Chanin Nantasenamat <141535473+sfc-gh-cnantasenamat@users.noreply.github.com>
2023-12-09 15:20:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,8 @@ import base64
 with open('bg-img.png', "rb") as f:
     data = f.read()
     encoded = base64.b64encode(data)
+    
 data = "data:image/png;base64," + encoded.decode("utf-8")
-
-
 
 
 def card1():
@@ -28,9 +27,9 @@ def card2():
           key='second'
         )
 
-with st.sidebar():
-    st.button('Button 1', on_click=card1)
-    st.button('Button 2', on_click=card2)
+
+st.button('Button 1', on_click=card1)
+st.button('Button 2', on_click=card2)
 
 
 
